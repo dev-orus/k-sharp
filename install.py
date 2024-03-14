10cd157adabd4c22e1509b087ee5f3b02e9c828d
Leo dev <klesti.selimay@icloud.com>
2024-03-14 12:08:44 +0100

    better installer

--- install.py
+++ install.py
@@ -3,7 +3,9 @@ import os
 if os.name == 'nt':...
 else:
     d = os.path.dirname(__file__)
-    with open('bin/ksharp', 'w')as f:
+    if not os.path.exists(os.path.join(d, '/bin/ksharp')):
+        os.mkdir(os.path.join(d, '/bin/ksharp'))
+    with open(os.path.join(d, '/bin/ksharp'), 'w')as f:
         f.write(f"""#!sh
 python3 {os.path.join(d, 'ksharp.py')} $1""")
     os.system('chmod +x '+os.path.join(d, 'bin', 'ksharp'))
